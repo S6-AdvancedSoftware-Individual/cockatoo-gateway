071e7047d1f521820c2de245a6aded16f17a93b1
theartcher <jorisplayz@gmail.com>
2025-03-31 17:08:13 +0200

    Add initial project structure with configuration, Docker support, and API routing

---
+++ CockatooGateway/Program.cs
@@ -0,0 +1,31 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config
+                .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                .AddJsonFile("ocelot.json")
+                .AddEnvironmentVariables();
+        })
+        .ConfigureServices(s => {
+            s.AddOcelot();
+        })
+        .UseIISIntegration()
+        .Configure(app =>
+        {
+            app.UseOcelot().Wait();
+        })
+        .Build()
+        .Run();
+    }
+}
