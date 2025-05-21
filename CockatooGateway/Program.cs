c575b3d08bfa99efc89fb66049bece09637317dd
theartcher <jorisplayz@gmail.com>
2025-05-21 12:42:56 +0200

    Enhance project configuration with health checks, logging, and rate limiting; update .gitignore and add docker-compose for deployment

--- CockatooGateway/Program.cs
+++ CockatooGateway/Program.cs
@@ -1,13 +1,34 @@
 using CockatooGateway;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
+var betterstack_sourceToken = Environment.GetEnvironmentVariable("BETTERSTACK_SOURCETOKEN") ?? "";
+var betterstack_endpoint = Environment.GetEnvironmentVariable("BETTERSTACK_ENDPOINT") ?? "";
+var auth0_domain = Environment.GetEnvironmentVariable("AUTH0_DOMAIN") ?? "";
+var auth0_audience = Environment.GetEnvironmentVariable("AUTH0_AUDIENCE") ?? "";
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Configure SeriLog as the global logger.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.BetterStack(
+        sourceToken: betterstack_sourceToken,
+        betterStackEndpoint: betterstack_endpoint
+    )
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog();
 
 // Configure app configuration
 builder.Configuration
@@ -28,37 +49,70 @@ builder.Services.AddCors(options =>
     });
 });
 
+// Configure security and authentication
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer("Auth0", options =>
 {
-    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
-    options.Audience = builder.Configuration["Auth0:Audience"];
+    options.Authority = $"https://{auth0_domain}/";
+    options.Audience = auth0_audience;
     options.TokenValidationParameters = new TokenValidationParameters
     {
         NameClaimType = ClaimTypes.NameIdentifier
     };
 });
 
+// Configure authorization policies
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("scp");
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Add("scp", "scope");
 
-
+// Configure authorization & health checks
 builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+builder.Services.AddHealthChecks();
 
 // Add services
 builder.Services.AddOcelot();
 
-
 var app = builder.Build();
+
+// Custom middleware to log unauthenticated requests
+app.Use(async (context, next) =>
+{
+    if (!context.User.Identity?.IsAuthenticated ?? true)
+    {
+        Log.Warning("Unauthenticated request: {Method} at '{Path}' from '{IP}'",
+            context.Request.Method,
+            context.Request.Path,
+            context.Connection.RemoteIpAddress);
+    }
+    await next();
+});
 
 // Use CORS middleware
 app.UseCors("AllowSpecificOrigins");
 
-// Configure middleware
+// Configure the HTTP request pipeline
 app.UseRouting();
-
-app.UseAuthentication();
 app.UseAuthorization();
+
+// Configure health checks
+app.MapHealthChecks("/_health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+// Add health endpoint as controller
+app.UseEndpoints(e =>
+{
+    e.MapControllers();
+});
+
+
+// Configure authentication and authorization
+app.UseAuthentication();
+
+app.MapControllers();
+
+// Configure Ocelot middleware
 app.UseOcelot().Wait();
 
 app.Run();
